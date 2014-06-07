7c184fc47dc7b6a7505075212e7d398a8e9a08e5
Manuel Hohmann <mhohmann@physnet.uni-hamburg.de>
2014-06-07 08:47:38 +0300

    Added functions for updating an existing database.

--- updatedb.php
+++ updatedb.php
@@ -5,10 +5,12 @@ $pdo = new PDO('mysql:host=HOSTNAME;port=3306;dbname=DBNAME;charset=utf8', 'USER
 // Enter list of countries to be imported. For every country there must be a directory with that name containing the TMC location code tables in TISA format.
 $countries = array('de'=>"Germany", 'it'=>"Italy", 'se'=>"Sweden", 'no'=>"Norway", 'fi'=>"Finland", 'fr'=>"France", 'be'=>"Belgium", 'es'=>"Spain");
 
-$pdo->exec("CREATE TABLE countries (cid INTEGER, tabcd INTEGER, name VARCHAR(100), dcomment VARCHAR(100), version VARCHAR(8), versiondescription VARCHAR(100), PRIMARY KEY (cid, tabcd)) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;");
+$tables = array('locationcodes', 'names', 'administrativearea', 'otherareas', 'roads', 'segments', 'soffsets', 'points', 'poffsets', 'intersections');
 
 foreach($countries as $country)
 {
+	$pdo->exec("DELETE FROM countries WHERE name = $country");
+
 	$charset = get_charset($country);
 
 	$csv = fopen("$country/LOCATIONDATASETS.DAT", "r");
@@ -26,18 +28,20 @@ foreach($countries as $country)
 		$vals = "('" . implode("', '", $data) . "')";
 		$query = "INSERT INTO countries $cols VALUES $vals;";
 		echo $pdo->exec($query) . ": $query\n";
+
+		foreach($tables as $table)
+			$pdo->exec("DELETE FROM $table WHERE cid = {$data['cid']}");
 	}
 
 	fclose($csv);
 }
 
-function create_table($file, $layout)
+function update_table($table)
 {
 	global $pdo;
 	global $countries;
 
-	$table = strtolower($file);
-	$pdo->exec("CREATE TABLE $table ($layout) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;");
+	$file = strtoupper($table);
 
 	foreach($countries as $country)
 	{
@@ -68,14 +72,12 @@ function create_table($file, $layout)
 	}
 }
 
-function create_types($table, $file, $layout)
+function update_types($table, $file)
 {
 	global $pdo;
 	global $countries;
 
 	$table = strtolower($table);
-
-	$pdo->exec("CREATE TABLE $table ($layout) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;");
 
 	foreach($countries as $cid=>$country)
 	{
@@ -84,9 +86,6 @@ function create_types($table, $file, $layout)
 		$csv = fopen("$country/$file.DAT", "r");
 		$header = explode(";", utf8_encode(trim(fgets($csv))));
 		$cols = strtolower("(" . implode(", ", $header) . ")");
-
-		// Add National COL
-		$pdo->exec("ALTER TABLE $table ADD desc_$cid VARCHAR(255);");
 
 		// International Cols
 		$headerInt = array('CLASS', 'TCD', 'STCD', '`DESC`');
@@ -129,15 +128,8 @@ function get_charset($country)
 	return $arr[0];
 }
 
-create_types('TYPES', 'SUBTYPES', 'class CHAR(1), tcd TINYINT(4), stcd TINYINT(4), `desc` VARCHAR(255), PRIMARY KEY (class, tcd, stcd)');
-create_table('LOCATIONCODES', 'cid INTEGER, tabcd INTEGER, lcd INTEGER, allocated BOOLEAN, PRIMARY KEY (cid, tabcd, lcd)');
-create_table('NAMES', 'cid INTEGER, lid INTEGER, nid INTEGER, name VARCHAR(255), ncomment TEXT, KEY cid (cid, nid)');
-create_table('ADMINISTRATIVEAREA', 'cid INTEGER, tabcd INTEGER, lcd INTEGER, class CHAR, tcd TINYINT, stcd TINYINT, nid INTEGER, pol_lcd INTEGER, PRIMARY KEY (cid, tabcd, lcd)');
-create_table('OTHERAREAS', 'cid INTEGER, tabcd INTEGER, lcd INTEGER, class CHAR, tcd TINYINT, stcd TINYINT, nid INTEGER, pol_lcd INTEGER, PRIMARY KEY (cid, tabcd, lcd)');
-create_table('ROADS', 'cid INTEGER, tabcd INTEGER, lcd INTEGER, class CHAR, tcd TINYINT, stcd TINYINT, roadnumber VARCHAR(16), rnid INTEGER, n1id INTEGER, n2id INTEGER, pol_lcd INTEGER, pes_lev INTEGER, PRIMARY KEY (cid, tabcd, lcd)');
-create_table('SEGMENTS', 'cid INTEGER, tabcd INTEGER, lcd INTEGER, class CHAR, tcd TINYINT, stcd TINYINT, roadnumber VARCHAR(16), rnid INTEGER, n1id INTEGER, n2id INTEGER, roa_lcd INTEGER, seg_lcd INTEGER, pol_lcd INTEGER, PRIMARY KEY (cid, tabcd, lcd)');
-create_table('SOFFSETS', 'cid INTEGER, tabcd INTEGER, lcd INTEGER, neg_off_lcd INTEGER, pos_off_lcd INTEGER, PRIMARY KEY (cid, tabcd, lcd)');
-create_table('POINTS', 'cid INTEGER, tabcd INTEGER, lcd INTEGER, class CHAR, tcd TINYINT, stcd TINYINT, junctionnumber VARCHAR(16), rnid INTEGER, n1id INTEGER, n2id INTEGER, pol_lcd INTEGER, oth_lcd INTEGER, seg_lcd INTEGER, roa_lcd INTEGER, inpos BOOLEAN, inneg BOOLEAN, outpos BOOLEAN, outneg BOOLEAN, presentpos BOOLEAN, presentneg BOOLEAN, diversionpos BOOLEAN, diversionneg BOOLEAN, xcoord REAL, ycoord REAL, interruptsroad INTEGER, urban BOOLEAN, PRIMARY KEY (cid, tabcd, lcd)');
-create_table('POFFSETS', 'cid INTEGER, tabcd INTEGER, lcd INTEGER, neg_off_lcd INTEGER, pos_off_lcd INTEGER, PRIMARY KEY (cid, tabcd, lcd)');
-create_table('INTERSECTIONS', 'cid INTEGER, tabcd INTEGER, lcd INTEGER, int_cid INTEGER, int_tabcd INTEGER, int_lcd INTEGER, PRIMARY KEY (cid, tabcd, lcd)');
+update_types('TYPES', 'SUBTYPES');
+
+foreach($tables as $table)
+	update_table($table);
 ?>
