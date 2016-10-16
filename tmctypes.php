1115c32728e207269c2c2463a6d0ad70b1e468e9
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2016-10-16 10:06:44 +0300

    Extended types view: group by table, show only those which are present and number of entries.

--- tmctypes.php
+++ tmctypes.php
@@ -18,15 +18,32 @@ function tmc_types()
 </head>
 <body>
 <h1>TMC types for country cid = <?php echo $cid; ?> and tabcd = <?php echo $tabcd; ?></h1>
-<table>
 <?php
-	$result = $pdo->query("SELECT * FROM types ORDER BY class, tcd, stcd ASC");
-	while($data = $result->fetch(PDO::FETCH_ASSOC))
+	$tables = array(
+		'administrativearea' => "Administrative areas",
+		'otherareas' => "Other areas",
+		'roads' => "Roads",
+		'segments' => "Segments",
+		'points' => "Points"
+	);
+
+	foreach($tables as $table => $tabname)
 	{
-		echo "<tr><td><a href=\"tmcview.php?cid=$cid&amp;tabcd=$tabcd&amp;class=" . $data['class'] . "&amp;tcd=" . $data['tcd'] . "&amp;stcd=" . $data['stcd'] . "\">" . $data['class'] . $data['tcd'] . "." . $data['stcd'] . "</a></td><td>" . $data['tdesc'] . "</td></tr>\n";
+		echo "<h2>$tabname</h2>\n";
+		echo "<table>\n";
+		echo "<tr><th>Type</th><th>Description</th><th>Entries</th></tr>\n";
+
+		$stmt = $pdo->prepare("SELECT types.*, COUNT(*) FROM types, $table WHERE types.class = $table.class AND types.tcd = $table.tcd AND types.stcd = $table.stcd AND $table.cid = :cid AND $table.tabcd = :tabcd GROUP BY types.class, types.tcd, types.stcd;");
+		$stmt->bindValue('cid', $cid, PDO::PARAM_INT);
+		$stmt->bindValue('tabcd', $tabcd, PDO::PARAM_INT);
+		$stmt->execute();
+
+		while($data = $stmt->fetch(PDO::FETCH_ASSOC))
+			echo "<tr><td><a href=\"tmcview.php?cid=$cid&amp;tabcd=$tabcd&amp;class={$data['class']}&amp;tcd={$data['tcd']}&amp;stcd={$data['stcd']}\">{$data['class']}{$data['tcd']}.{$data['stcd']}</a></td><td>{$data['tdesc']}</td><td>{$data['COUNT(*)']}</td></tr>\n";
+
+		echo "</table>\n";
 	}
 ?>
-</table>
 <?php form_search(); ?>
 </body>
 </html>
